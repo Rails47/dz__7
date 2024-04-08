dd405b9f3a2bc564993d1f3cf60f64585171ae27
Rails47 <158601407+Rails47@users.noreply.github.com>
2024-04-08 14:47:08 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient client = new UdpClient();
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Broadcast, 12345);
+            client.EnableBroadcast = true;
+
+            client.Client.Bind(new IPEndPoint(IPAddress.Any, 0));
+            while (true)
+            {
+                byte[] receivedData = client.Receive(ref endPoint);
+                string receivedText = Encoding.ASCII.GetString(receivedData);
+                Console.WriteLine("Current Time: " + receivedText);
+            }
+        }
+    }
+}
